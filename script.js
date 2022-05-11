3617ebaaa2a34fbd7f11a5916f550aaa3631f9f5
magnoo94 <magnis1994@gmail.com>
2022-05-10 20:02:25 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+let opcion = document.getElementById("jugadores", "equiposDeFutbol", "cantidadDeGoles")
+jugadores.innerHTML += "<option> Lionel Messi </option>"
+jugadores.innerHTML += "<option> Cristiano Ronaldo </option>"
+jugadores.innerHTML += "<option> Robert Lewandoski </option>"
+jugadores.innerHTML += "<option> Karim Benzema </option>"
+
+equiposDeFutbol.innerHTML += "<option> Paris-Saint germain   </option>"
+equiposDeFutbol.innerHTML += "<option> Manchester United </option>"
+equiposDeFutbol.innerHTML += "<option> Bayern Munich </option>"
+equiposDeFutbol.innerHTML += "<option> Real Madrid </option>"
+
+cantidadDeGoles.innerHTML += "<option>  1 </option>"
+cantidadDeGoles.innerHTML += "<option>  2 </option>"
+cantidadDeGoles.innerHTML += "<option>  3 </option>"
+cantidadDeGoles.innerHTML += "<option>  4 </option>"
